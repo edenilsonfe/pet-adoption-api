b83ef7621a4bc575400f251ae43cdc5de1fb28d8
Edenilson Ferreira <edenilson.ferreira00@gmail.com>
2020-01-22 00:06:38 -0300

    add django_rest_framework

--- pet_adoption/settings.py
+++ pet_adoption/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
 
     'pets',
     'organizations'
@@ -126,6 +127,13 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_RENDERER_CLASSES': [
+        'rest_framework.renderers.JSONRenderer',
+        'rest_framework.renderers.BrowsableAPIRenderer',
+    ]
+}
 
 
 # Internationalization
